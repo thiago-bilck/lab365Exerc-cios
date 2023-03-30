5eab90a06aab91ee514478f5695cc399aef613d2
thiago-bilck <thiagobilck@gmail.com>
2023-03-30 17:37:23 -0300

    feat: add Ex7  from week 6

--- Semana 6/Ex6/routes.js
+++ Semana 6/Ex6/routes.js
@@ -3,11 +3,37 @@ const router = express.Router();
 
 
 router.post("/user", (req,res)=>{
-    res.send("Usuário criado")
+    
+    const {nome, idade, cargo, senha} = req.body
+   // res.send("Usuário criado")
+
+   if (nome === undefined || idade === undefined || cargo === undefined || senha === undefined){
+    res.status(406)
+    return res.json({"mensagem": "Está faltando dados para concluir a operação"})
+   }
+    else if(idade<21){
+        return res.json({"mensagem":"Usuário não possui idade suficiente"})
+    }
+    else {
+        res.status(200)
+        return res.json({"mensagem":"Criado com sucesso"})
+    }
+    console.log(nome)
 })
 
 router.delete("/user", (req, res) =>{
-    res.send("Usuário deletado")
+    const {id} = req.query
+
+    //res.send("Usuário deletado")
+    if (id === undefined || id === null || id ===""){
+        res.status(406)
+        return res.json({"mensagem":"Estão faltando dados para concluir a operação"})
+    }
+    else {
+        res.status(200)
+        return res.json({"mensagem":"Deletado com sucesso"})
+
+    }
 })
 
 module.exports = router;
--- Semana 6/Ex6/server.js
+++ Semana 6/Ex6/server.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const router = require("./routes")
 
+app.use(express.json())
+
+
 app.use('/', router)
 
 
