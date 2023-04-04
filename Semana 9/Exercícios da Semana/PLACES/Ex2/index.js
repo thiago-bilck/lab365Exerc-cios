fc5761ed2eab5ef80d8656d19d4799851a85bf2e
thiago-bilck <thiagobilck@gmail.com>
2023-04-04 17:49:05 -0300

    feat: create Ex 6 from week 9

--- Semana 9/Exercícios da Semana/PLACES/Ex2/index.js
+++ Semana 9/Exercícios da Semana/PLACES/Ex2/index.js
@@ -43,6 +43,38 @@ app.get('/places', async (req,res) => {
 
 })
 
+app.put('/places/:id', async (req,res) => {
+
+    console.log(req.params.id);
+    console.log(req.body);
+    try {
+        const placeInDatabase = await Place.findByPk(req.params.id)//esta é uma maneira melhor do que fazer Place.findOne({where: {id: req.params.id}})
+   
+        if(!placeInDatabase){
+            return res
+            .status(404)
+            .json({message: 'ID não encontrado'})
+            
+        }
+
+        placeInDatabase.name = req.body.name
+        placeInDatabase.description = req.body.description
+        placeInDatabase.contact = req.body.contact
+        placeInDatabase.opening_hours = req.body.opening_hours
+        placeInDatabase.latitud = req.body.latitud
+        placeInDatabase.longitud = req.body.longitud
+        //caso eu não atualize algum dado, o proprio sequelize entende que ele não foi alterado e deixa como estava, ao invés de dar um undefined
+
+        await placeInDatabase.save()//update de tudo que foi alterado
+
+        res.json(placeInDatabase)
+
+
+    } catch (error) {
+        
+    }
+})
+
 
 app.listen(9999, () => {
     console.log("Servidor funcionando")
