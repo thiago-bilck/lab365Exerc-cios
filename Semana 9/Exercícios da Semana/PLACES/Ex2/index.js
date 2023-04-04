470797c92d26aaf4467b93089dffb1a61e4d6327
thiago-bilck <thiagobilck@gmail.com>
2023-04-04 18:00:16 -0300

    feat: creat Ex 5 from week 9

--- Semana 9/Exercícios da Semana/PLACES/Ex2/index.js
+++ Semana 9/Exercícios da Semana/PLACES/Ex2/index.js
@@ -75,6 +75,37 @@ app.put('/places/:id', async (req,res) => {
     }
 })
 
+app.delete('/places/:id', async (req,res) => {
+
+    console.log(req.params)
+
+    if(!req.params.id){
+        return res
+        .status(406)
+        .json({message: 'id não encontrado'})
+    }
+
+
+    const placeInDatabase = await Place.findByPk(req.params.id)
+   
+        if(!placeInDatabase){
+            return res
+            .status(404)
+            .json({message: 'ID não encontrado'})
+            
+        }
+
+    //neste caso não criamos uma variável para armazenar a opção delete pois como o mesmo já diz, queremos deletar o dado
+    await Place.destroy({
+        where: {
+            id: req.params.id,
+        }
+    })
+
+    res.status(200).json({message: 'deletado com sucesso'})
+
+})
+
 
 app.listen(9999, () => {
     console.log("Servidor funcionando")
